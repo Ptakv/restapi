60470d667a661e564d6dd003d843a4c27affe4fc
Ptakv <ptakv1722@gmail.com>
2021-02-24 18:29:54 +0100

    first commit

---
+++ src/main/java/com/myfirstwebapi/restapi/Post.java
@@ -0,0 +1,19 @@
+package com.myfirstwebapi.restapi;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Post 
+{
+  public int id;
+  public String title;
+  public String content;
+  public Post(
+  @JsonProperty("id") int id,
+  @JsonProperty("title") String title,
+  @JsonProperty("content") String content)
+  {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+  }
+}
---
+++ src/main/java/com/myfirstwebapi/restapi/PostController.java
@@ -0,0 +1,21 @@
+  
+package com.myfirstwebapi.restapi;
+
+import java.io.IOException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController 
+{
+  @GetMapping("/hello")
+  public String Hello() 
+  {
+    return "Hello world";
+  }
+  @GetMapping("/addPost")
+  public Post[] AddPost(String title, String content) throws IOException 
+  {
+    return JsonActions.addPost(title,content);
+  }
+}
